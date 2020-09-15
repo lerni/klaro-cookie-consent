defd900d9350e151127802f538008bbf4fba8438
Lukas Erni <le@kraftausdruck.ch>
2020-09-15 22:33:42 +0200

    expose scripts only if enabled

--- src/KlaroInitExtension.php
+++ src/KlaroInitExtension.php
@@ -4,7 +4,10 @@ class KlaroInitExtension extends Extension
 {
     public function contentControllerInit($controller)
     {
-        Requirements::insertHeadTags('<script type="application/javascript" src="/_klaro-config/"></script>');
-        Requirements::javascript('klaro-cookie-consent/dist/klaro.js');
+        $siteConfig = SiteConfig::current_site_config();
+        if ($siteConfig->CookieIsActive) {
+            Requirements::insertHeadTags('<script type="application/javascript" src="/_klaro-config/"></script>');
+            Requirements::javascript('klaro-cookie-consent/dist/klaro.js');
+        }
     }
 }
