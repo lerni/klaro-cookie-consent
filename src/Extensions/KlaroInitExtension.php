45ca82b5e13d66e0d2b78db5b71502d2825fb8e6
Lukas Erni <le@kraftausdruck.ch>
2020-09-15 22:33:40 +0200

    expose scripts only if enabled

--- src/Extensions/KlaroInitExtension.php
+++ src/Extensions/KlaroInitExtension.php
@@ -4,13 +4,17 @@ namespace  Kraftausdruck\Extensions;
 
 use SilverStripe\Core\Extension;
 use SilverStripe\View\Requirements;
+use SilverStripe\SiteConfig\SiteConfig;
 use SilverStripe\Core\Manifest\ModuleResourceLoader;
 
 class KlaroInitExtension extends Extension
 {
     public function onBeforeInit()
     {
-        Requirements::javascript('/_klaro-config/');
-        Requirements::javascript(ModuleResourceLoader::resourceURL('lerni/klaro-cookie-consent:dist/klaro.js'));
+        $siteConfig = SiteConfig::current_site_config();
+        if ($siteConfig->CookieIsActive) {
+            Requirements::javascript('/_klaro-config/');
+            Requirements::javascript(ModuleResourceLoader::resourceURL('lerni/klaro-cookie-consent:dist/klaro.js'));
+        }
     }
 }
