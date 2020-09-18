ecd20500ccf69c5a554067b0124fec1174e42421
Lukas Erni <le@kraftausdruck.ch>
2020-09-18 16:28:04 +0200

    cachebuster hash lang & siteconfig for config (not cookie nor cat ATM)

--- src/KlaroInitExtension.php
+++ src/KlaroInitExtension.php
@@ -5,8 +5,9 @@ class KlaroInitExtension extends Extension
     public function contentControllerInit($controller)
     {
         $siteConfig = SiteConfig::current_site_config();
+        $hash = substr(md5($siteConfig->LastEdited . i18n::get_locale()), 0, 12);
         if ($siteConfig->CookieIsActive) {
-            Requirements::insertHeadTags('<script type="application/javascript" src="/_klaro-config/"></script>');
+            Requirements::insertHeadTags('<script type="application/javascript" src="/_klaro-config/?v='.$hash.'"></script>');
             Requirements::javascript('klaro-cookie-consent/dist/klaro.js');
         }
     }
