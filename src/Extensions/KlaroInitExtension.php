c2d07c269ab77681bf1cac2f6bbf7d116c5a9732
Lukas Erni <le@kraftausdruck.ch>
2022-04-26 18:56:14 +0200

    cachebuster also on preconnect, conditional requirements

--- src/Extensions/KlaroInitExtension.php
+++ src/Extensions/KlaroInitExtension.php
@@ -16,30 +16,32 @@ class KlaroInitExtension extends Extension
         $siteConfig = SiteConfig::current_site_config();
         $preconnect = Config::inst()->get('Kraftausdruck\Extensions\KlaroInitExtension', 'preconnect');
 
-        if ($siteConfig->CookieIsActive && $preconnect === 'true' && $this->owner->response) {
-            $additionalLinkHeaders = [
-                '</_klaro-config/>; rel=preload; as=script',
-                sprintf(
-                    '<%s>; rel=preload; as=style',
-                    ModuleResourceLoader::resourceURL('/_resources/vendor/lerni/klaro-cookie-consent/node_modules/klaro/dist/klaro.min.css')
-                ),
-                sprintf(
-                    '<%s>; rel=preload; as=script',
-                    ModuleResourceLoader::resourceURL('/_resources/vendor/lerni/klaro-cookie-consent/node_modules/klaro/dist/klaro-no-css.js')
-                )
-            ];
-            $headers = $this->owner->response->getHeaders();
-            if (array_key_exists('link', $headers)) {
-                $linkHeaders = explode(',', $headers['link']);
-                $linkHeaders = array_merge($linkHeaders, $additionalLinkHeaders);
-            } else {
-                $linkHeaders = $additionalLinkHeaders;
+        if ($siteConfig->CookieIsActive && $this->owner->response) {
+            $hash = substr(md5($siteConfig->LastEdited . i18n::get_locale()), 0, 12);
+            if ($preconnect === 'true') {
+                $additionalLinkHeaders = [
+                    '</_klaro-config/?m=' . $hash . '>; rel=preload; as=script',
+                    sprintf(
+                        '<%s>; rel=preload; as=style',
+                        ModuleResourceLoader::resourceURL('/_resources/vendor/lerni/klaro-cookie-consent/node_modules/klaro/dist/klaro.min.css')
+                    ),
+                    sprintf(
+                        '<%s>; rel=preload; as=script',
+                        ModuleResourceLoader::resourceURL('/_resources/vendor/lerni/klaro-cookie-consent/node_modules/klaro/dist/klaro-no-css.js')
+                    )
+                ];
+                $headers = $this->owner->response->getHeaders();
+                if (array_key_exists('link', $headers)) {
+                    $linkHeaders = explode(',', $headers['link']);
+                    $linkHeaders = array_merge($linkHeaders, $additionalLinkHeaders);
+                } else {
+                    $linkHeaders = $additionalLinkHeaders;
+                }
+                $this->owner->response->addHeader('link', implode(',', $linkHeaders));
             }
-            $this->owner->response->addHeader('link', implode(',', $linkHeaders));
+            Requirements::css(ModuleResourceLoader::resourceURL('lerni/klaro-cookie-consent:node_modules/klaro/dist/klaro.min.css'));
+            Requirements::javascript('/_klaro-config/?m=' . $hash);
+            Requirements::javascript(ModuleResourceLoader::resourceURL('lerni/klaro-cookie-consent:node_modules/klaro/dist/klaro-no-css.js'));
         }
-        $hash = substr(md5($siteConfig->LastEdited . i18n::get_locale()), 0, 12);
-        Requirements::css(ModuleResourceLoader::resourceURL('lerni/klaro-cookie-consent:node_modules/klaro/dist/klaro.min.css'));
-        Requirements::javascript('/_klaro-config/?m=' . $hash);
-        Requirements::javascript(ModuleResourceLoader::resourceURL('lerni/klaro-cookie-consent:node_modules/klaro/dist/klaro-no-css.js'));
     }
 }
