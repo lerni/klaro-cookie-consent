175c6f06e54c97d59003888d6e90bf4f19aeef67
Lukas Erni <le@kraftausdruck.ch>
2025-06-18 18:24:45 +0200

    tweak tasks

--- src/Tasks/CreateKlaroDefaults.php
+++ src/Tasks/CreateKlaroDefaults.php
@@ -16,122 +16,117 @@ class KlaroDefaults extends BuildTask
     protected string $title = 'Manage Klaro! Records';
     protected static string $description = 'Creates defaults and export entries';
 
-    private static $segment = 'klaro-defaults-export';
+    protected static string $commandName = 'klaro-defaults';
 
     protected function execute(InputInterface $input, PolyOutput $output): int
     {
-        try {
-            $modulePath = dirname(dirname(dirname(__FILE__)));
+        $modulePath = dirname(dirname(dirname(__FILE__)));
 
-            // In SS6 BuildTask, we can't easily add custom options
-            // Use current i18n locale or default locale
-            $locale = i18n::get_locale() ?: i18n::config()->get('default_locale');
+        $locale = i18n::get_locale() ?: i18n::config()->get('default_locale');
+        $language = i18n::getData()->langFromLocale($locale);
 
-            $language = i18n::getData()->langFromLocale($locale);
+        $output->writeln("Using locale: {$locale}");
+        $output->writeln("Language: {$language}");
+        $output->writeln("RFC1766: " . i18n::convert_rfc1766($locale));
 
-            $output->writeln("Using locale: {$locale}");
-            $output->writeln("Language: {$language}");
-            $output->writeln("RFC1766: " . i18n::convert_rfc1766($locale));
+        // Check for translations from Klaro
+        $translationsFile = $modulePath . '/all-translations-from-klaro.yml';
 
-            // Check for translations from Klaro
-            $translationsFile = $modulePath . '/all-translations-from-klaro.yml';
-            $defaults_from_klaro = [];
-
-            if (!file_exists($translationsFile)) {
-                $output->writeln("<error>Translation file not found: {$translationsFile}</error>");
-                return Command::FAILURE;
-            }
-
-            $allTranslations = Yaml::parseFile($translationsFile);
-
-            if (!isset($allTranslations[$language])) {
-                $output->writeln("<warning>No translation entries found for language: {$language}</warning>");
-                return Command::SUCCESS; // Not an error, just no translations available
-            }
-
-            $defaults_from_klaro = $allTranslations[$language];
-            $output->writeln("Found " . count($defaults_from_klaro) . " translation entries for language: {$language}");
-
-            // Create mapping between Klaro translations and SiteConfig fields
-            $klaroToSiteConfigMapping = [
-                'acceptAll' => 'AcceptAll',
-                'acceptSelected' => 'AcceptSelected',
-                'decline' => 'Decline',
-                'ok' => 'ConsentNoticeOK',
-                'consentNotice.learnMore' => 'ConsentNoticeLearnMore',
-                'consentNotice.title' => 'ConsentNoticeTitle',
-                'consentNotice.description' => 'ConsentNoticeDescription',
-                'consentModal.title' => 'ConsentModalTitle',
-                'consentModal.description' => 'ConsentModalDescription',
-                'privacyPolicy.name' => 'ConsentModalPrivacyPolicyName',
-                'privacyPolicy.text' => 'ConsentModalPrivacyPolicyText'
-            ];
-
-            // Helper function to get nested array value by dot notation
-            $getNestedValue = function($array, $key) {
-                $keys = explode('.', $key);
-                $value = $array;
-                foreach ($keys as $k) {
-                    if (isset($value[$k])) {
-                        $value = $value[$k];
-                    } else {
-                        return null;
-                    }
-                }
-                return $value;
-            };
-
-            // Get SiteConfig and prepare defaults from Klaro
-            $siteConfig = SiteConfig::current_site_config();
-            $siteConfigNeedsWrite = false;
-
-            // First, set values from Klaro translations
-            foreach ($klaroToSiteConfigMapping as $klaroKey => $siteConfigField) {
-                $klaroValue = $getNestedValue($defaults_from_klaro, $klaroKey);
-                if ($klaroValue && empty($siteConfig->{$siteConfigField})) {
-                    $siteConfig->{$siteConfigField} = $klaroValue;
-                    $siteConfigNeedsWrite = true;
-                    $output->writeln("Set {$siteConfigField} from Klaro: {$klaroValue}");
-                }
-            }
-
-            // Override with custom SilverStripe translations if available
-            $customDefaults = [
-                'ConsentNoticeOK' => _t('Kraftausdruck\KlaroCookie.ConsentNoticeOK', "That's ok"),
-                'ConsentNoticeLearnMore' => _t('Kraftausdruck\KlaroCookie.ConsentNoticeLearnMore', 'Let me choose'),
-                'ConsentNoticeTitle' => _t('Kraftausdruck\KlaroCookie.ConsentNoticeTitle', 'Cookie Consent'),
-                'ConsentNoticeDescription' => _t('Kraftausdruck\KlaroCookie.ConsentNoticeDescription', 'Hi! Could we please enable some additional services for {purposes}? You can always change or withdraw your consent later.'),
-                'ConsentModalTitle' => _t('Kraftausdruck\KlaroCookie.ConsentModalTitle', 'Services we would like to use'),
-                'ConsentModalDescription' => _t('Kraftausdruck\KlaroCookie.ConsentModalDescription', "Here you can assess and customize the services that we'd like to use on this website. You're in charge! Enable or disable services as you see fit."),
-                'ConsentModalPrivacyPolicyName' => _t('Kraftausdruck\KlaroCookie.ConsentModalPrivacyPolicyName', 'privacy policy'),
-                'ConsentModalPrivacyPolicyText' => _t('Kraftausdruck\KlaroCookie.ConsentModalPrivacyPolicyText', 'To learn more, please read our {privacyPolicy}.'),
-                'AcceptAll' => _t('Kraftausdruck\KlaroCookie.AcceptAll', 'Accept all'),
-                'AcceptSelected' => _t('Kraftausdruck\KlaroCookie.AcceptSelected', 'Accept selected'),
-                'Decline' => _t('Kraftausdruck\KlaroCookie.Decline', 'I decline')
-            ];
-
-            // Override Klaro values with custom translations if they exist
-            foreach ($customDefaults as $field => $customValue) {
-                if (!empty($customValue) && $customValue !== $field) {
-                    $siteConfig->{$field} = $customValue;
-                    $siteConfigNeedsWrite = true;
-                    $output->writeln("Overrode {$field} with custom translation: {$customValue}");
-                }
-            }
-
-            // Write SiteConfig if any changes were made
-            if ($siteConfigNeedsWrite) {
-                $siteConfig->write();
-                $output->writeln('<info>Updated SiteConfig with Klaro defaults and custom translations</info>');
-            } else {
-                $output->writeln('<comment>No SiteConfig updates needed - all fields already have values</comment>');
-            }
-
-            return Command::SUCCESS;
-
-        } catch (\Exception $e) {
-            $output->writeln("<error>Error executing KlaroDefaults task: " . $e->getMessage() . "</error>");
+        if (!file_exists($translationsFile)) {
+            $output->writeln("<error>Translation file not found: {$translationsFile}</error>");
             return Command::FAILURE;
         }
+
+        try {
+            $allTranslations = Yaml::parseFile($translationsFile);
+        } catch (\Exception $e) {
+            $output->writeln("<error>Failed to parse translation file: " . $e->getMessage() . "</error>");
+            return Command::FAILURE;
+        }
+
+        if (!isset($allTranslations[$language])) {
+            $output->writeln("<warning>No translation entries found for language: {$language}</warning>");
+            return Command::SUCCESS; // Not an error, just no translations available
+        }
+
+        $defaults_from_klaro = $allTranslations[$language];
+        $output->writeln("Found " . count($defaults_from_klaro) . " translation entries for language: {$language}");
+
+        // Create mapping between Klaro translations and SiteConfig fields
+        $klaroToSiteConfigMapping = [
+            'acceptAll' => 'AcceptAll',
+            'acceptSelected' => 'AcceptSelected',
+            'decline' => 'Decline',
+            'ok' => 'ConsentNoticeOK',
+            'consentNotice.learnMore' => 'ConsentNoticeLearnMore',
+            'consentNotice.title' => 'ConsentNoticeTitle',
+            'consentNotice.description' => 'ConsentNoticeDescription',
+            'consentModal.title' => 'ConsentModalTitle',
+            'consentModal.description' => 'ConsentModalDescription',
+            'privacyPolicy.name' => 'ConsentModalPrivacyPolicyName',
+            'privacyPolicy.text' => 'ConsentModalPrivacyPolicyText'
+        ];
+
+        // Helper function to get nested array value by dot notation
+        $getNestedValue = function($array, $key) {
+            $keys = explode('.', $key);
+            $value = $array;
+            foreach ($keys as $k) {
+                if (isset($value[$k])) {
+                    $value = $value[$k];
+                } else {
+                    return null;
+                }
+            }
+            return $value;
+        };
+
+        // Get SiteConfig and prepare defaults from Klaro
+        $siteConfig = SiteConfig::current_site_config();
+        $siteConfigNeedsWrite = false;
+
+        // First, set values from Klaro translations
+        foreach ($klaroToSiteConfigMapping as $klaroKey => $siteConfigField) {
+            $klaroValue = $getNestedValue($defaults_from_klaro, $klaroKey);
+            if ($klaroValue && empty($siteConfig->{$siteConfigField})) {
+                $siteConfig->{$siteConfigField} = $klaroValue;
+                $siteConfigNeedsWrite = true;
+                $output->writeln("Set {$siteConfigField} from Klaro: {$klaroValue}");
+            }
+        }
+
+        // Override with custom SilverStripe translations if available
+        $customDefaults = [
+            'ConsentNoticeOK' => _t('Kraftausdruck\KlaroCookie.ConsentNoticeOK', "That's ok"),
+            'ConsentNoticeLearnMore' => _t('Kraftausdruck\KlaroCookie.ConsentNoticeLearnMore', 'Let me choose'),
+            'ConsentNoticeTitle' => _t('Kraftausdruck\KlaroCookie.ConsentNoticeTitle', 'Cookie Consent'),
+            'ConsentNoticeDescription' => _t('Kraftausdruck\KlaroCookie.ConsentNoticeDescription', 'Hi! Could we please enable some additional services for {purposes}? You can always change or withdraw your consent later.'),
+            'ConsentModalTitle' => _t('Kraftausdruck\KlaroCookie.ConsentModalTitle', 'Services we would like to use'),
+            'ConsentModalDescription' => _t('Kraftausdruck\KlaroCookie.ConsentModalDescription', "Here you can assess and customize the services that we'd like to use on this website. You're in charge! Enable or disable services as you see fit."),
+            'ConsentModalPrivacyPolicyName' => _t('Kraftausdruck\KlaroCookie.ConsentModalPrivacyPolicyName', 'privacy policy'),
+            'ConsentModalPrivacyPolicyText' => _t('Kraftausdruck\KlaroCookie.ConsentModalPrivacyPolicyText', 'To learn more, please read our {privacyPolicy}.'),
+            'AcceptAll' => _t('Kraftausdruck\KlaroCookie.AcceptAll', 'Accept all'),
+            'AcceptSelected' => _t('Kraftausdruck\KlaroCookie.AcceptSelected', 'Accept selected'),
+            'Decline' => _t('Kraftausdruck\KlaroCookie.Decline', 'I decline')
+        ];
+
+        // Override Klaro values with custom translations if they exist
+        foreach ($customDefaults as $field => $customValue) {
+            if (!empty($customValue) && $customValue !== $field) {
+                $siteConfig->{$field} = $customValue;
+                $siteConfigNeedsWrite = true;
+                $output->writeln("Overrode {$field} with custom translation: {$customValue}");
+            }
+        }
+
+        // Write SiteConfig if any changes were made
+        if ($siteConfigNeedsWrite) {
+            $siteConfig->write();
+            $output->writeln('<info>Updated SiteConfig with Klaro defaults and custom translations</info>');
+        } else {
+            $output->writeln('<comment>No SiteConfig updates needed - all fields already have values</comment>');
+        }
+
+        return Command::SUCCESS;
     }
 }
--- src/Tasks/LangFilesFromNPM.php
+++ src/Tasks/LangFilesFromNPM.php
@@ -13,65 +13,85 @@ use Symfony\Component\Console\Input\InputInterface;
 // it's meant to be run on dev-env only
 class LangFilesFromNPM extends BuildTask
 {
-    protected static string $description = 'generates language files based on klaro yml files';
-    private static $segment = 'gen-lang-files';
+    protected string $title = 'Generate Language Files from NPM';
+    protected static string $description = 'generates language files based on klaro yml files (requires npm packages to be installed)';
+    protected static string $commandName = 'gen-lang-files';
 
 	protected function execute(InputInterface $input, PolyOutput $output): int
     {
-        $this->generateLangFiles();
+        $modulePath = dirname(dirname(dirname(__FILE__)));
+
+        // Check if source directory exists
+        $sourceDir = $modulePath .'/client/node_modules/klaro/src/translations';
+        if (!is_dir($sourceDir)) {
+            $output->writeln("<error>{$sourceDir} DOESN'T EXIST --- please run npm install first</error>");
+            return Command::FAILURE;
+        }
+
+        $output->writeln("Reading translations from: {$sourceDir}");
+        $filesFromKlaro = glob($sourceDir . '/*.yml');
+
+        if (empty($filesFromKlaro)) {
+            $output->writeln("<warning>No translation files found in {$sourceDir}</warning>");
+            return Command::SUCCESS;
+        }
+
+        $allTranslations = [];
+        $processedCount = 0;
+
+        foreach ($filesFromKlaro as $file) {
+            try {
+                $filecontent = Yaml::parseFile($file);
+                $lang = $this->extractLanguageFromFilename($file);
+
+                // Add this language's translations to the combined array
+                $allTranslations[$lang] = $filecontent;
+                $processedCount++;
+                $output->writeln("Processed: {$lang}");
+
+            } catch (\Exception $e) {
+                $output->writeln("<error>Failed to parse {$file}: " . $e->getMessage() . "</error>");
+                continue;
+            }
+        }
+
+        if ($processedCount > 0) {
+            // Write all translations to a single file
+            $outputFile = $modulePath .'/all-translations-from-klaro.yml';
+            try {
+                $allTranslationsYML = Yaml::dump($allTranslations);
+                file_put_contents($outputFile, $allTranslationsYML);
+                $output->writeln("<info>Successfully wrote: {$outputFile} with {$processedCount} languages</info>");
+            } catch (\Exception $e) {
+                $output->writeln("<error>Failed to write output file: " . $e->getMessage() . "</error>");
+                return Command::FAILURE;
+            }
+        } else {
+            $output->writeln("<warning>No files were processed successfully</warning>");
+        }
+
         return Command::SUCCESS;
     }
 
-    function generateLangFiles()
-	{
+    /**
+     * Extract language code from filename and handle special cases
+     */
+    private function extractLanguageFromFilename(string $file): string
+    {
+        $lang = basename($file, '.yml');
 
-        $modulePath = dirname(dirname(dirname(__FILE__)));
-        // $lang = Locale::getPrimaryLanguage(i18n::get_locale());
-
-        // Check if source directory exists
-        // $sourceDir = $modulePath .'/node_modules/klaro/src/translations';
-        $sourceDir = $modulePath .'/client/node_modules/klaro/src/translations';
-        if (!is_dir($sourceDir)) {
-            DB::alteration_message($sourceDir . ' DOESN\'T EXIST --- please run npm install first', "error");
-            return;
+        // Handle special cases
+        // sr@latin.yml vs sr.yml & sr_cyrl.yml
+        if ($lang == 'sr') {
+            $lang = 'sr@latin';
+        }
+        if ($lang == 'sr_cyrl') {
+            $lang = 'sr';
+        }
+        if ($lang[0] == '_') {
+            $lang = ltrim($lang, '_');
         }
 
-        $filesFromKlaro = glob($sourceDir . '/*.yml');
-
-        $allTranslations = [];
-        $i = 0;
-        foreach ($filesFromKlaro as $file) {
-            $filecontent = Yaml::parseFile($file);
-            $lang = explode('/', $file);
-            $lang = end($lang);
-            $lang = explode('.', $lang);
-            $lang = $lang[0];
-
-            // gahh special case
-            // sr@latin.yml vs sr.yml & sr_cyrl.yml
-            if ($lang == 'sr') {
-                $lang = 'sr@latin';
-            }
-            if ($lang == 'sr_cyrl') {
-                $lang = 'sr';
-            }
-            if ($lang[0] == '_') {
-                $lang = ltrim($lang, '_');
-            }
-
-            // Add this language's translations to the combined array
-            $allTranslations[$lang] = $filecontent;
-            $i++;
-        }
-
-        if ($i > 0) {
-            // Write all translations to a single file
-            $allTranslationsYML = Yaml::dump($allTranslations);
-            $outputFile = $modulePath .'/all-translations-from-klaro.yml';
-            file_put_contents($outputFile, $allTranslationsYML);
-            DB::alteration_message('wrote: '. $outputFile . ' with ' . $i . ' languages', "error");
-        } else {
-            DB::alteration_message('no file imported!', "info");
-        }
+        return $lang;
     }
 }
