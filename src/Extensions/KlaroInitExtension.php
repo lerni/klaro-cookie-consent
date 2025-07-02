c7b5fb0417b953e23423a913501d17656acc2d0e
Lukas Erni <le@kraftausdruck.ch>
2025-07-02 13:50:06 +0200

    remove slashes _klaro-config

--- src/Extensions/KlaroInitExtension.php
+++ src/Extensions/KlaroInitExtension.php
@@ -30,7 +30,7 @@ class KlaroInitExtension extends Extension
             $hash = substr(md5(implode('|', $hashComponents)), 0, 12);
             if ($preconnect === 'true') {
                 $additionalLinkHeaders = [
-                    '</_klaro-config/?m=' . $hash . '>; rel=preload; as=script',
+                    '<_klaro-config?m=' . $hash . '>; rel=preload; as=script',
                     sprintf(
                         '<%s>; rel=preload; as=style',
                         ModuleResourceLoader::resourceURL('lerni/klaro-cookie-consent:client/node_modules/klaro/dist/klaro.min.css')
@@ -50,7 +50,7 @@ class KlaroInitExtension extends Extension
                 $this->owner->response->addHeader('link', implode(',', $linkHeaders));
             }
             Requirements::css(ModuleResourceLoader::resourceURL('lerni/klaro-cookie-consent:client/node_modules/klaro/dist/klaro.min.css'));
-            Requirements::javascript('/_klaro-config/?m=' . $hash);
+            Requirements::javascript('_klaro-config?m=' . $hash);
             Requirements::javascript(ModuleResourceLoader::resourceURL('lerni/klaro-cookie-consent:client/node_modules/klaro/dist/klaro-no-css.js'));
         }
     }
