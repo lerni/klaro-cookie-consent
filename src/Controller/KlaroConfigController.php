08e88b5488d39acdbf854358755eb889aa8814b2
3Dgoo <michael@3dgoo.com>
2025-07-10 00:07:44 +0930

    Update KlaroConfigController.php

--- src/Controller/KlaroConfigController.php
+++ src/Controller/KlaroConfigController.php
@@ -4,7 +4,7 @@ namespace Kraftausdruck\Controller;
 
 use SilverStripe\i18n\i18n;
 use SilverStripe\Model\List\ArrayList;
-use SilverStripe\View\ArrayData;
+use SilverStripe\Model\ArrayData;
 use TractorCow\Fluent\Model\Locale;
 use SilverStripe\Control\Controller;
 use SilverStripe\Control\HTTPRequest;
